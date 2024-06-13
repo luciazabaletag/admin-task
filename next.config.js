4261722ad405443d060fb37a6cfd190236deaea4
luciazabaletag <luciazabaletag@gmail.com>
2024-06-13 11:23:35 +0200

    url prod

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 
 const apiUrl = {
-    develop: 'http://localhost:3000/api'
+    develop: 'http://localhost:3000/api',
+    prod: 'https://admin-task-zeta.vercel.app/api',
   };
   
   const nextConfig = {
     env: {
-      apiUrl: apiUrl.develop
+      apiUrl: apiUrl.prod || apiUrl.develop
     },
     reactStrictMode: false,
     typescript: {
